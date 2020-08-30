9e23463d0e1c5613e4b1895cd1d909d4e676435e
Cristian Moreno <cristian.moreno@avoristravel.com>
2020-08-30 12:20:52 +0200

    #master
    Initial commit

---
+++ src/main/java/com/statio/pirates/proof/StratioProofPiratesApplication.java
@@ -0,0 +1,13 @@
+package com.statio.pirates.proof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratioProofPiratesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StratioProofPiratesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/statio/pirates/proof/StratioProofPiratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statio.pirates.proof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratioProofPiratesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
