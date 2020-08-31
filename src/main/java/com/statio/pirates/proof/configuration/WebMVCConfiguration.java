bff04ed92a1181be196ac77bbc0c61ad9d0c1ae3
Cristian Moreno <cristian.moreno@avoristravel.com>
2020-08-31 21:25:44 +0200

    #master
    - Configuration swagger

---
+++ src/main/java/com/statio/pirates/proof/configuration/WebMVCConfiguration.java
@@ -0,0 +1,30 @@
+package com.statio.pirates.proof.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/ui/**").addResourceLocations("classpath:/META-INF/resources/ui/");
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+        registry.addViewController("/").setViewName("forward:/swagger-ui.html");
+    }
+}
